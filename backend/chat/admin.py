e5a9741d8c5e30fedfa4fa72c3ca1a71542ef2ae
Fenil Nagar <fenilnagar001@gmail.com>
2026-06-23 23:29:22 +0530

    register Room and Message models in Django admin

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Room, Message
 
-# Register your models here.
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'created_at']
+
+    list_filter = ['created_at']
+
+@admin.register(Message)
+class MessageAmin(admin.ModelAdmin):
+
+    list_display = ['id', 'sender', 'room', 'timestamp']
+
+    search_fields = ['sender__username', 'content']
+
+    list_filter= ['timestamp']
