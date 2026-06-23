33c79d4c3bb34c4c0d9ef3f958be46ece7c5007b
Fenil Nagar <fenilnagar001@gmail.com>
2026-06-23 23:01:07 +0530

    add Room and Message models

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Room Model - represents chat between two users
+class Room(models.Model):
+
+    members = models.ManyToManyField(
+        User,
+        related_name='rooms'
+    )
+
+    created_at =  models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Room: {', '.join([user.username for user in self.members.all()])}"
+
+
+# Message Model - represents a single message inside a room
+class Message(models.Model):
+    
+    room = models.ForeignKey(
+        Room,
+        on_delete = models.CASCADE,
+        related_name='messages'
+    )
+
+    sender = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name='messages'
+    )
+
+    content = models.TextField()
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['timestamp']
+    
+    def __str__(self):
+        return f"{self.sender.username}: {self.content[:50]}"
+
+
+    
